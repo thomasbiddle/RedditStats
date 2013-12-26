04c56a2604fc6baf84e323067c602bac36856a8e
TJ Biddle <tjbiddle@inflection.com>
2013-12-26 01:30:47 -0500

    Rails init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditStats::Application.config.secret_key_base = '31d3f473aa633e173939f6dd2caaf82e0d2b2d8170ede27eb2fe00335f6dad8c69856363ae3cb2d06cd644d12ab8fe3d79aecd2a9d28477f17aaa98940265f9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RedditStats::Application.config.session_store :cookie_store, key: '_RedditStats_session'
